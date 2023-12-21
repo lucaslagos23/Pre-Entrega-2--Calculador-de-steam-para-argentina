2e6df7b15ffe6f5694390da287d4aa96435ab33b
lucas <lucasagustinlagos@hotmail.com>
2023-12-21 13:13:05 -0300

    Incluyendo objetos

--- pages/main.js
+++ pages/main.js
@@ -1,4 +1,42 @@
-alert("¡Bienvenido al Mundo de Impuestos Gamer! Tu recorrido comienza aquí, explorando las tarifas fiscales de los juegos en Argentina. ¡Calcula tus impuestos y asegúrate de estar listo para tu próxima compra!")
+const mostrarJuegos = (juegos, tipoJuego, numero) => {
+    alert(`A continuación se le mostrarán los juegos ${tipoJuego} disponibles:`);
+    juegos.forEach((juego, index) => {
+        alert(`${index + numero}: ${juego.nombre}`);
+    });
+};
+
+const tiposDeJuegos = {
+    RPG: [//Objeto de juegosRPG
+        { id: 1, nombre: "Elden Ring", precio: 47.99 },
+        { id: 2, nombre: "Baldur's gate 3", precio: 34.99 },
+        { id: 3, nombre: "Starfield", precio: 41.99 },
+        { id: 4, nombre: "Cyberpunk 2077", precio: 44.99 },
+        { id: 5, nombre: "Sea of Stars", precio: 17.99 },
+    ],
+    SHOOTER: [//Objeto de juegosSHOOTER
+        { id: 6, nombre: "Doom Eternal", precio: 23.99 },
+        { id: 7, nombre: "Halo", precio: 39.99 },
+        { id: 8, nombre: "Far cry 4", precio: 23.99 },
+        { id: 9, nombre: "Battlefield 1", precio: 39.99 },
+        { id: 10, nombre: "Black Mesa", precio: 19.99 },
+    ],
+};
+
+const mensajeDeBienvenidaYDespedida = {//creo un objeto que tendra dentro un saludo con el mensaje de bienvenida y despedida.
+    saludo: () => alert("¡Bienvenido al Mundo de Impuestos Gamer! Tu recorrido comienza aquí, explorando las tarifas fiscales de los juegos en Argentina. ¡Calcula tus impuestos y asegúrate de estar listo para tu próxima compra!"),
+    despedida: (nombreUsuario) => alert(`¡Gracias ${nombreUsuario} por usar el Calculador de Impuestos de Steam! ¡Hasta pronto!`),
+};
+
+const impuestos = [//Creo un arrays con los impuestos , en caso que se agreguen otro solo lo pongo por debajo.
+    { nombre: "Impuesto PAIS", porcentaje: 0.3 },
+    { nombre: "Percepción de Impuesto a las Ganancias", porcentaje: 0.3 },
+];
+
+mensajeDeBienvenidaYDespedida.saludo();
+
+
+
+
 
 let nuevoUsuario = prompt("¿Desea crear un nombre de usuario para proseguir?SI/NO").toLowerCase();
 if (nuevoUsuario === "no") {
