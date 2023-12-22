42aedf303d1d6701eb7611b5a843dc34791605b3
lucas <lucasagustinlagos@hotmail.com>
2023-12-22 10:19:10 -0300

    Agregando array y metodos

--- pages/main.js
+++ pages/main.js
@@ -34,10 +34,6 @@ const impuestos = [//Creo un arrays con los impuestos , en caso que se agreguen 
 
 mensajeDeBienvenidaYDespedida.saludo();
 
-
-
-
-
 let nuevoUsuario = prompt("¿Desea crear un nombre de usuario para proseguir?SI/NO").toLowerCase();
 if (nuevoUsuario === "no") {
     alert("¡Hasta pronto! Esperamos verte de vuelta muy pronto. ¡Que tengas un buen día!");
@@ -47,37 +43,53 @@ if (nuevoUsuario === "no") {
     alert("Bienvenido " + nombreDeUsuario + " a tu Calculador de Impuestos de Steam");
 
     while (true) {
-        let precioJuego = parseFloat(prompt("Ingrese el valor en dólares del juego al que desea calcular sus impuestos"));// Con esta variable el usuario me indica el valor del juego que quiere calcularte el impuesto.
+        mostrarJuegos(tiposDeJuegos.RPG, "RPG", 1);
+        mostrarJuegos(tiposDeJuegos.SHOOTER, "Shooter", 6);
 
-        if (isNaN(precioJuego)) {//Con esto me aseguro que el usuario no ingrese letras , solo un valor numerico.
-            alert("Por favor, ingresa un valor numérico válido.");
-        }else {
-            function calculo(tipoCambio = 969) {
-            let conversionADolar = precioJuego * tipoCambio;//PrecioDolar a PesoArgentino
-            let bienesPersonales =0.25; //Impuesto 1
-            let impuestoPais=0.3//Impuesto 2
-            let impuestosALasGanancias=1.00 //Impuesto 3
-            let precioConImpuesto = conversionADolar + (conversionADolar * (bienesPersonales + impuestoPais + impuestosALasGanancias));
-            //Precio total en pesos, sumandole los impuestos.
-    
-            //Se le muestra al usuario el precio en pesos y precio en dolares con sus impuestos aplicados.
-            alert("El total de su juego en pesos es $" + conversionADolar + "\nEl precio con impuestos es: $" +  precioConImpuesto);
+        let opcionElegida = parseInt(prompt("Ingrese el número del juego que desea calcular sus impuestos"));
 
-            //Alert para mostrarle el tipo de cambio utilizado y los impuestos regidos por steam que se le sumaron al precio del juego.
-            alert ("Cotización del dólar tarjeta: 1 USD ≈"+tipoCambio+
-            "\nEsta cotización referencial es provista por steam e incluye todos los impuestos listados abajo."+
-            "\n"+
-            "\nImpuestos Nacionales (Boletín Oficial)".toUpperCase()+ 
-            "\nPercepción de Bienes Personales- RG AFIP Nº 5430/2023 25%"+
-            "\nImpuesto PAIS - RG AFIP N° 4659/2020 30%"+
-            "\nPercepción de Impuesto a las Ganancias - RG AFIP Nº 5232/2022 100%)");
+        let juegoSeleccionado = tiposDeJuegos.RPG.find(juego => juego.id === opcionElegida) || tiposDeJuegos.SHOOTER.find(juego => juego.id === opcionElegida);
+        //Uso find para que me busque con la funcion el primer id del juego.
+        if (!juegoSeleccionado) {
+            alert("Opción no válida. Por favor, elija un número de juego válido.");
+            continue;
         }
-        calculo(); 
-        let nuevoPrecio = prompt("¿Desea realizar otro cálculo? SI/NO").toLowerCase();
-        if (nuevoPrecio !== "si") {//Le indica un true y se ejecuta el ciclo de nuevo.
-            alert("¡Gracias "+ nombreDeUsuario +" "+ "por usar el Calculador de Impuestos de Steam! ¡Hasta pronto!");
-            break; //Se corta el ciclo cuando dice que no.
+
+        const tipoCambio = 842.68; // Precio del dolar (Cierre del 22/12/2023-10:08)
+
+        const conversionDePesoADolar = juegoSeleccionado.precio * tipoCambio; //PrecioDolar a PesoArgentino
+
+        const impuestoPais = impuestos.find(impuesto => impuesto.nombre === "Impuesto PAIS").porcentaje;
+        const impuestosALasGanancias = impuestos.find(impuesto => impuesto.nombre === "Percepción de Impuesto a las Ganancias").porcentaje;
+
+        const precioConImpuesto =
+            conversionDePesoADolar +
+            conversionDePesoADolar * (impuestoPais + impuestosALasGanancias); //Precio total en pesos, sumandole los impuestos.
+
+        const impuestosTotales =
+            precioConImpuesto * (impuestoPais + impuestosALasGanancias);
+
+        //Se le muestra al usuario el precio total con impuestos , el monto de impuestos a pagar y total sin impuestos.
+        alert(`Total con impuestos de ${juegoSeleccionado.nombre} es $${precioConImpuesto.toFixed(2)} 
+        Impuestos totales: $${impuestosTotales.toFixed(2)} 
+        Total sin impuestos: $${conversionDePesoADolar.toFixed(2)}`);
+        //Coloco el toFixed para que no me tome solo los dos ultimos numeros despues del punto, asi no tener que redondear ni para arriba ni para abajo, dando el precio exacto.
+
+        //Alert para mostrarle el tipo de cambio utilizado y los impuestos regidos por steam que se le sumaron al precio del juego.
+        alert(`Cotización del dólar tarjeta: 1 USD ≈ ${tipoCambio} ARS
+        Esta cotización referencial es provista por steam e incluye todos los impuestos listados abajo.
+
+        ${impuestos.map(impuesto => `
+        ${impuesto.nombre} ${impuesto.porcentaje * 100}%`).join('\n')}`);
+        //Con el map mapeo los impuestos del objeto impuestos dejandome un string , a este lo multiplico x 100 para que me muestre el porcentaje no los decimales de los impuestos,luego uso el join para unir todos estos elementos en una sola cadena y le egrego el salto de linea para que no se vean uno al lado del otro sino uno abajo del otro.
+
+        const nuevoPrecio = prompt(
+            "¿Desea realizar otro cálculo? SI/NO"
+        ).toLowerCase();
+        if (nuevoPrecio !== "si") {
+            mensajeDeBienvenidaYDespedida.despedida(nombreDeUsuario);
+            break;
         }
     }
-}
+
 }
